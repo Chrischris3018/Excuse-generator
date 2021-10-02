4963a40857dbbc75f491e62ef20437bf6c300c3f
Chrischris3018 <chrischrischris3018@yahoo.com>
2021-10-02 00:25:53 +0000

    finished

--- app.js
+++ app.js
@@ -3,4 +3,28 @@ let action = ['ate','peed','crushed','broke'];
 let what = ['my homework', 'the keys', 'the car'];
 let when = ['before the class','right on time','when I finished','during my lunch','while I was praying'];
 
-var quoteRandom = Math.floor(Math.random() * 8)
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min) + min);
+}
+
+const type1 = function(){
+    const who1 = who[ getRandomInt(0,who.length -1 ) ];
+    const action2 = action[getRandomInt(0,action.length -1)];
+    const what3 = what[getRandomInt(0,what.length -1)];
+    const when4 = when[getRandomInt(0,when.length -1)];
+
+    return `${who1} ${action2} ${what3} ${when4}`;
+}
+
+function type2 (){
+    console.log(what[2])    
+}
+
+type2()
+const type3 = () => {
+    console.log(what[1])
+}
+
+type3()
