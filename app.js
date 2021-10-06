7c99909a8c2c128d12ec8c3e6d94f34be55bd327
Chrischris3018 <chrischrischris3018@yahoo.com>
2021-10-06 22:48:53 +0000

    done

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min) + min);
 }
 
-const type1 = function(){
+const excuseGen = function(){
     const who1 = who[ getRandomInt(0,who.length -1 ) ];
     const action2 = action[getRandomInt(0,action.length -1)];
     const what3 = what[getRandomInt(0,what.length -1)];
@@ -17,14 +17,3 @@ const type1 = function(){
 
     return `${who1} ${action2} ${what3} ${when4}`;
 }
-
-function type2 (){
-    console.log(what[2])    
-}
-
-type2()
-const type3 = () => {
-    console.log(what[1])
-}
-
-type3()
